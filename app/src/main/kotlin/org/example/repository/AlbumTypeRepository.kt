0caca29323264161570adc05ab439e940567aab8
jeoliveira045 <jean.mateus.1997@gmail.com>
2024-10-13 08:17:42 -0300

    Ajuste do AlbumType

--- app/src/main/kotlin/org/example/repository/AlbumTypeRepository.kt
+++ app/src/main/kotlin/org/example/repository/AlbumTypeRepository.kt
@@ -1,7 +1,6 @@
 package org.example.repository
 
 import org.example.entities.AlbumType
-import org.example.rest.AlbumTypeRest
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
